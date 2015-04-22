b61eb5712e3e61de2ee3bd23dda81cbad5a9d6df
mfidino <mfidino@lpzoo.org>
2015-04-22 10:10:16 -0500

    small update reflecting function changes

--- simul_prep_run.R
+++ simul_prep_run.R
@@ -27,16 +27,19 @@ sim_df <- make_sim_df(nspec = 5, nsite = 118, nyear = 5, nrep = 28,
                       gam = c(.1, .5, .9), phi = c(.1, .5, .9),
                       p = c(.1, .5, .9), gam_sd = c(.1, 1, 3),
                       phi_sd = c(.1, 1, 3), p_sd = c(.1, 1, 3),
-                      add_NA = FALSE, percent_to_NA = 0.2, row_replicate = 1)
+                      add_NA = TRUE, percent_to_NA = 0.2, row_replicate = 1)
 # for our comparison stuff
 sim_df <- make_sim_df(nspec = 5, nsite = 118, nyear = 5, nrep = 28,
                       gam = .4, phi = .7, p = .1, gam_sd = 1,
-                      phi_sd = 1, p_sd = 1, add_NA = FALSE, percent_to_NA = 0.2,
+                      phi_sd = 1, p_sd = 1, add_NA = TRUE, percent_to_NA = 0.2,
+                      cut_off = 20,
                       row_replicate = 10)
 
 
 
 # test gens 2 simulations
+
+#Note: takes ~0.5 seconds to run for each row of sim_df
 
 all_sim <- simulate_from_sim_df(sim_df, test = TRUE)
 
@@ -57,7 +60,7 @@ params <- c("gam",
 
   
 start_time <- Sys.time()
-batch_analyze(all_sim = list(all_sim[[1]]), params = params, n_chains = 4,adapt_steps = 3000,
+batch_analyze(all_sim = list(all_sim[[1]]), params = params, n_chains = 3,adapt_steps = 3000,
                 burn_in = 15000, sample_steps = 25000, thin_steps = 1,
               make_comparisons = TRUE)
 end_time <- Sys.time()
