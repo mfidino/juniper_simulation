c31a8aa6d9b213e5142203337dab51756c8aca5f
Mason Fidino <mfidino@lpzoo.org>
2015-12-10 17:00:56 -0600

    update Bayesian models
    putting bayesian models up

--- beta_model.R
+++ beta_model.R
@@ -18,12 +18,6 @@ model{
     sigma_phi~dunif(0,10)
     tau_phi<- pow(sigma_phi, -2)
     
-    # interaction hyperparameters
-    #p_int ~ dbeta(2,2)
-    #mu_int <- log(p_int / (1 - p_int))
-    sigma_int~dunif(0, 10)
-    tau_int <- pow(sigma_int, -2)
-    
     # p hyperparameters
     p_p ~ dbeta(1, 1)
     mu_p <- log(p_p / (1 - p_p))
@@ -38,39 +32,31 @@ model{
     
     #### occupancy model, specify the formulation of random effects here
     # species specific random effects
-    
-    for (i in 1:(nspec)){
-      gam[i] ~ dnorm(mu_gam, tau_gam)
-      lp[i] ~ dnorm(mu_p, tau_p)
-      beta[i] ~ dunif(-10, 10)
-      beta2[i] ~ dunif(-10, 10)
-      spe_phi[i] ~ dnorm(mu_phi, tau_phi)
+    for (i in 1:(nspec)) {
+    gam[i] ~ dnorm(mu_gam, tau_gam)
+    phi[i] ~ dnorm(mu_phi, tau_phi)
+    lp[i] ~ dnorm(mu_p, tau_p)
+    dist2cent_gam[i] ~ dunif(-10, 10)
+    dist2cent_phi[i] ~ dunif(-10, 10)
+    area_gam[i] ~ dunif(-10, 10)
+    area_phi[i] ~ dunif(-10, 10)
     }
-    
-    
-      for(k in 1:(only_inxs)){
-      int_phi[k] ~ dnorm(0, tau_int)
-      }
-    
-      for(i in 1:(n_inxs)){
-          phi[rows_vec[i],cols_vec[i]] <- equals(rows_vec[i], cols_vec[i]) * spe_phi[rows_vec[i]]+
-                                          (rows_vec[i] != cols_vec[i]) * int_phi[inxs_vec[i]]
-            
-           # ifelse(equals(rows_vec[i], cols_vec[i]),
-                 #                                spe_phi[rows_vec[i]], int_phi[inxs_vec[i]])
-        }
-      
-      
-      
 
+    
+    
+    
+    
 
     for(i in 1:(nspec)){
     for(k in 1:(nsite)){
     z[i, k, 1] ~ dbern(psi_in[i])
     for(t in 2:(nyear)){
     logit(psi[i, k, t]) <-  gam[i] * (1 - z[i, k, t-1]) + 
-      (beta[i] * cov[k] * (1 - z[i, k, t-1])) +
-       (inprod(phi[i,], z[, k, t-1])* z[i, k, t-1]) + (beta2[i] * cov[k] *z[i, k, t-1]) 
+      (dist2cent_gam[i] * cov[k,1] * (1 - z[i, k, t-1])) +
+      (area_gam[i] * cov[k,2] * (1 - z[i, k, t-1])) +
+      phi[i] * z[i, k, t-1] + 
+      (dist2cent_phi[i] * cov[k,1] *z[i, k, t-1]) +
+      (area_phi[i] * cov[k,2] * z[i, k, t-1])
     z[i, k, t] ~ dbern(psi[i, k, t])
     }
     }
@@ -95,7 +81,16 @@ model{
     }
     }
 
-
+   # for(k in 1:(nsite)){
+    #  for(t in 1:(nyear)){
+   #   sp_rich[k,t] <- sum(z[,k,t])
+  #}
+#}
+  #for(i in 1:(nspec)){
+  #  for(p in 1:100){
+#beta_est[i,p] <- exp(gam[i] + beta[i] * pred_cov[p]) / (1 + exp(gam[i] + beta[i] * pred_cov[p]))
+    #}
+  #}
 
 
 
